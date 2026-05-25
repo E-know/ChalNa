522106e824c43c8a256a513cee660e92f4560ec3
E-know <eknow25@gmail.com>
2026-05-25 17:05:15 +0900

    ♻️ refactor: TCA 통합을 위한 AppRouter 재구조화
    AppRouter를 handler 기반으로 변경하여 TCA 의 path 와 연결.
    Feature view 들의 기존 push/pop/popToRoot 호출 호환성 유지.

--- Modules/AppCore/Sources/AppRouter.swift
+++ Modules/AppCore/Sources/AppRouter.swift
@@ -1,21 +1,24 @@
 import Observation
 
 /// 루트 NavigationStack의 라우트 열거형. Home은 루트에서 렌더하므로 스택에 push하지 않음.
-public enum Route: Hashable {
+public enum Route: Hashable, Sendable {
     case mediaPicker
     case timeline
     case export
 }
 
+/// AppFeature(TCA) 의 path 와 연결되는 thin wrapper.
+/// Feature view 들에서 기존 `router.push/pop/popToRoot` 호출 호환을 위해 유지.
+/// RootView 가 onAppear 시 handler 3 개를 store.send 로 와이어한다.
 @Observable
 public final class AppRouter {
-    public var path: [Route]
+    public var pushHandler: ((Route) -> Void)?
+    public var popHandler: (() -> Void)?
+    public var popToRootHandler: (() -> Void)?
 
-    public init(path: [Route] = []) {
-        self.path = path
-    }
+    public init() {}
 
-    public func push(_ route: Route) { path.append(route) }
-    public func pop() { if !path.isEmpty { path.removeLast() } }
-    public func popToRoot() { path.removeAll() }
+    public func push(_ route: Route) { pushHandler?(route) }
+    public func pop() { popHandler?() }
+    public func popToRoot() { popToRootHandler?() }
 }
